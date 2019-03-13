056702fcbaf14843002175c04c17bc5dc3edb571
Chris Hold <chris.hold@mailbox.tu-berlin.de>
2019-03-13 15:26:16 +0100

    DOCS: Show path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,8 +14,10 @@
 #
 import os
 import sys
-sys.path.insert(0, os.path.abspath('.'))
-sys.path.append(os.path.abspath('../../spaudiopy'))
+
+sys.path.insert(0, os.path.abspath('..'))
+sys.path.append(os.path.abspath('../../'))
+print('\n'.join(sys.path))
 
 # -- Project information -----------------------------------------------------
 
