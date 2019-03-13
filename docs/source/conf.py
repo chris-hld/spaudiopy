f256f6a0ee0a41de0df176708c3121fdde82210e
Chris Hold <chris.hold@mailbox.tu-berlin.de>
2019-03-13 20:00:55 +0100

    Fix conda

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,11 +15,11 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath('..'))
-sys.path.append(os.path.abspath('../../'))
-sys.path.append(os.path.abspath('./'))
+#sys.path.insert(0, os.path.abspath('..'))
+#sys.path.append(os.path.abspath('../../'))
+#sys.path.append(os.path.abspath('./'))
 # sys.path.append(os.path.abspath('../../spaudiopy'))
-print('\n'.join(sys.path))
+#print('\n'.join(sys.path))
 
 # -- Project information -----------------------------------------------------
 
