41081beecd3f08ef2ac09ed543376cd9eff94d88
Chris Hold <chris.hold@mailbox.tu-berlin.de>
2019-03-13 20:34:35 +0100

    Add paths again

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,10 +15,10 @@
 import os
 import sys
 
-#sys.path.insert(0, os.path.abspath('..'))
-#sys.path.append(os.path.abspath('../../'))
-#sys.path.append(os.path.abspath('./'))
-# sys.path.append(os.path.abspath('../../spaudiopy'))
+sys.path.insert(0, os.path.abspath('..'))
+sys.path.append(os.path.abspath('../../'))
+sys.path.append(os.path.abspath('./'))
+sys.path.append(os.path.abspath('../../spaudiopy'))
 #print('\n'.join(sys.path))
 
 # -- Project information -----------------------------------------------------
