e58976e2df8792b8a9d8ad5dd50d05c2880f990f
Chris Hold <chris.hold@mailbox.tu-berlin.de>
2019-07-01 16:22:15 +0300

    Include precode explicitly

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -78,7 +78,7 @@ napoleon_use_rtype = False
 plot_include_source = True
 plot_html_show_source_link = False
 plot_html_show_formats = False
-plot_pre_code = None
+plot_pre_code = 'import numpy as np; from matplotlib import pyplot as plt'
 plot_rcparams = {
     'savefig.bbox': 'tight',
     }
