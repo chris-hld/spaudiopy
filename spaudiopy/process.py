115532e7502e3002268b26ebfa964b17dbe3968b
Chris Hold <christoph.hold@aalto.fi>
2020-05-13 16:36:39 +0300

    cleanup haversine from process

--- spaudiopy/process.py
+++ spaudiopy/process.py
@@ -88,43 +88,6 @@ def resample_hrirs(hrir_l, hrir_r, fs_hrir, fs_target, jobs_count=1):
     return hrir_l_resampled, hrir_r_resampled, fs_hrir
 
 
-def haversine(azi1, colat1, azi2, colat2, r=1):
-    """Calculate the spherical distance between two points on the sphere.
-    The spherical distance is central angle for r=1.
-
-    Parameters
-    ----------
-    azi1 : (n,) array_like
-    colat1 : (n,) array_like.
-    azi2 : (n,) array_like
-    colat2: (n,) array_like
-    r : float, optional.
-
-    Returns
-    -------
-    c : (n,) array_like
-        Haversine distance between pairs of points.
-
-    Reference
-    ---------
-    https://en.wikipedia.org/wiki/Haversine_formula
-
-    """
-    lat1 = np.pi / 2 - colat1
-    lat2 = np.pi / 2 - colat2
-
-    dlon = azi2 - azi1
-    dlat = lat2 - lat1
-
-    haversin_A = np.sin(dlat / 2) ** 2
-    haversin_B = np.sin(dlon / 2) ** 2
-
-    haversin_alpha = haversin_A + np.cos(lat1) * np.cos(lat2) * haversin_B
-
-    c = 2 * r * np.arcsin(np.sqrt(haversin_alpha))
-    return c
-
-
 def match_loudness(sig_in, sig_target):
     """
     Match loundess of input to target, based on RMS and avoid clipping.
