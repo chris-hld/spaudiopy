f74578fba0771c9c0375e13e78614c25a97a68d6
Chris Hold <Christoph.Hold@aalto.fi>
2022-04-26 14:19:48 +0300

    Use all cores to resample HRIRs

--- spaudiopy/process.py
+++ spaudiopy/process.py
@@ -35,7 +35,7 @@ memory = Memory(cachedir)
 
 
 @memory.cache
-def resample_hrirs(hrir_l, hrir_r, fs_hrir, fs_target, jobs_count=1):
+def resample_hrirs(hrir_l, hrir_r, fs_hrir, fs_target, jobs_count=None):
     """
     Resample HRIRs to new SamplingRate(t), using multiprocessing.
 
