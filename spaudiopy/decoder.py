ce21e74b80591c34ad47e88c0d5f072e52504996
Chris Hold <Christoph.Hold@aalto.fi>
2022-05-12 18:10:17 +0300

    Introduce  MagLS Binaural Ambisonics Decoder

--- spaudiopy/decoder.py
+++ spaudiopy/decoder.py
@@ -27,7 +27,7 @@ import numpy as np
 import scipy.spatial as scyspat
 from scipy import signal
 
-from spaudiopy import utils, sph, IO, plots, grids
+from spaudiopy import utils, sph, sig, IO, plots, grids
 
 shared_array = None
 
@@ -1310,6 +1310,147 @@ def sh2bin(sig_nm, hrirs_nm):
     out_r = np.sum(signal.oaconvolve(sig_nm, np.squeeze(hrirs_nm[1, :, :]),
                                      axes=-1), axis=0)
     return np.vstack((out_l, out_r))
+
+
+def magls_bin(hrirs, N_sph, f_trans=None, hf_cont='angle', hf_delay=(0, 0)):
+    """Magnitude Least-Squares (magLS) binaural decoder.
+
+    This binaural decoder renders the (least squares) binaural output below
+    `f_trans`, while rendering a magnitude solution above.
+
+
+    Parameters
+    ----------
+    hrirs : sig.HRIRs
+        HRIR set.
+    N_sph : int
+        Ambisonic (SH) order.
+    f_trans : float, optional
+        Transition frequency between linear and magLS handling.
+        The default is None, which sets it to 'N_sph * 500'.
+    hf_cont : ['delay', 'avg', 'angle'], optional
+        High Frequency phase continuation method . The default is 'angle'.
+    hf_delay : (2,), optional
+        High frequency (additional) group delay in smpls. The default is (0, 0).
+
+    Raises
+    ------
+    ValueError
+        When passing not supported option.
+
+    Returns
+    -------
+    hrirs_mls_nm : (2, (N_sph+1)**2, L)
+        Decoding IRs matrix, 2: left,right (stacked), real coeffs, L taps.
+
+    Notes
+    -----
+    The iterative procedure in [1] suffers form HF dispersion (available by
+    `hf_cont='delay'` and `hf_delay=(0, 0))`.
+    This function offers multiple options to mitigate this issue. E.g. manually
+    estimating and setting `hf_delay`, or estimating a phase difference on
+    previous frequency bins which are then used to predict.
+    This delta can be the spherical average `hf_cont='avg'`, which offers an
+    algorithmic way to estimate the global group delay.
+    It can also be estimated per direction when `hf_cont='angle'`, which is
+    able to preserve group delay changes over the angle, however, might
+    reintroduce more complexity again.
+    For very low orders, there might be a slight tradeoff.
+
+    References
+    ----------
+    [1] Zotter, F., & Frank, M. (2019). Ambisonics. Springer Topics in Signal
+    Processing.
+
+    See Also
+    --------
+    :py:func:`spaudiopy.decoder.sh2bin` : Decode Ambisonic streams to binaural.
+    """
+    assert(isinstance(hrirs, sig.HRIRs))
+    if f_trans is None:
+        f_trans = N_sph * 500  # from N > kr
+    fs = hrirs.fs
+    hrirs_l = hrirs.left
+    hrirs_r = hrirs.right
+    azi = hrirs.grid['azi']
+    zen = hrirs.grid['colat']
+
+    numSmpls = hrirs.left.shape[1]
+    nfftmin = 1024
+    nfft = np.max([nfftmin, numSmpls])
+    freqs = np.fft.rfftfreq(nfft, 1/fs)
+
+    hrtfs_l = np.fft.rfft(hrirs_l, n=nfft)
+    hrtfs_r = np.fft.rfft(hrirs_r, n=nfft)
+
+    k_cuton = np.argmin(np.abs(freqs - f_trans))
+
+    Y = sph.sh_matrix(N_sph, azi, zen, 'real')
+    Y_pinv = np.linalg.pinv(Y)
+
+    hrtfs_mls_nm = np.zeros((2, (N_sph+1)**2, len(freqs)), dtype=hrtfs_l.dtype)
+    phi_l_mod = np.zeros((hrirs.grid_points, len(freqs)))
+    phi_r_mod = np.zeros((hrirs.grid_points, len(freqs)))
+    # TODO: weights, transition, order dependent
+
+    # linear part
+    hrtfs_mls_nm[0, :, :k_cuton] = Y_pinv @ hrtfs_l[:, :k_cuton]
+    hrtfs_mls_nm[1, :, :k_cuton] = Y_pinv @ hrtfs_r[:, :k_cuton]
+    phi_l_mod[:, :k_cuton] = np.angle(Y @ hrtfs_mls_nm[0, :, :k_cuton])
+    phi_r_mod[:, :k_cuton] = np.angle(Y @ hrtfs_mls_nm[1, :, :k_cuton])
+
+    if hf_cont == 'avg':
+        # get the delta (from prediction frame)
+        n_delta = 5
+        assert(k_cuton > n_delta)
+        # from spat avg
+        delta_phi_l = np.mean(np.diff(np.unwrap(np.angle(
+            hrtfs_mls_nm[0, 0, :k_cuton]))[k_cuton-n_delta-1:k_cuton-1]))
+        delta_phi_r = np.mean(np.diff(np.unwrap(np.angle(
+            hrtfs_mls_nm[1, 0, :k_cuton]))[k_cuton-n_delta-1:k_cuton-1]))
+    elif hf_cont == 'angle':
+        # get the delta (from prediction frame)
+        n_delta = 5
+        assert(k_cuton > n_delta)
+        # for each angle
+        delta_phi_l = np.mean(np.diff(np.unwrap(
+            phi_l_mod)[:, k_cuton-n_delta-1:k_cuton-1]), axis=-1)
+        delta_phi_r = np.mean(np.diff(np.unwrap(
+            phi_r_mod)[:, k_cuton-n_delta-1:k_cuton-1]), axis=-1)
+    elif hf_cont == 'delay':
+        delta_phi_l = 0
+        delta_phi_r = 0
+    else:
+        raise ValueError("hf_cont method not implemented")
+    # apply (additional) group hf delay
+    delta_w = 2*np.pi*freqs[1]
+    delta_phi_l = delta_phi_l - delta_w * (hf_delay[0] / fs)
+    delta_phi_r = delta_phi_r - delta_w * (hf_delay[1] / fs)
+
+    # manipulate above k_cuton
+    for k in range(k_cuton, len(freqs)):
+        phi_l_mod[:, k] = np.angle(Y @ hrtfs_mls_nm[0, :, k-1])
+        phi_r_mod[:, k] = np.angle(Y @ hrtfs_mls_nm[1, :, k-1])
+
+        # forward predict phase
+        phi_l_mod[:, k] = phi_l_mod[:, k] + delta_phi_l
+        phi_r_mod[:, k] = phi_r_mod[:, k] + delta_phi_r
+
+        # transform
+        hrtfs_mls_nm[0, :, k] = Y_pinv @ (np.abs(hrtfs_l[:, k]) *
+                                          np.exp(1j*phi_l_mod[:, k]))
+        hrtfs_mls_nm[1, :, k] = Y_pinv @ (np.abs(hrtfs_r[:, k]) *
+                                          np.exp(1j*phi_r_mod[:, k]))
+
+    hrirs_mls_nm = np.fft.irfft(hrtfs_mls_nm)
+    hrirs_mls_nm = hrirs_mls_nm[..., :numSmpls]
+
+    # taper last samples
+    taper_taps = 32
+    hrirs_mls_nm[:, :, -taper_taps:] *= np.hanning(2*taper_taps)[taper_taps:]
+    # taper first ?
+    hrirs_mls_nm[:, :, :4] *= np.hanning(8)[:4]
+    return hrirs_mls_nm
 
 
 # Parallel worker stuff -->
