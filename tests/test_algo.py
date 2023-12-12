785c789e7bcd0bb6202777ff3149420a10acd2b7
Benjamin Stahl <benjamin.stahl@kug.ac.at>
2023-12-12 13:49:33 +0100

    adds rotation matrices

--- tests/test_algo.py
+++ tests/test_algo.py
@@ -47,3 +47,41 @@ def test_calculate_grid_weights(test_n_sph):
 
     # Perfect Reconstruction
     assert_allclose(q_weights_t, q_weights)
+
+
+
+# use integer angles here (these definitely will not lead to special cases in
+# which a rotation matches by chance)
+@pytest.mark.parametrize(('test_n_sph', 'test_yaw', 'test_pitch', 'test_roll'),
+                         (N_SPHS, [1, 3, 5], [1, 3, 5], [1, 3, 5]))
+def test_rotation(test_n_sph, test_yaw, test_pitch, test_roll):
+    for N_sph in test_n_sph:
+        for yaw in test_yaw:
+            for pitch in test_pitch:
+                for roll in test_roll:
+                    tgrid = spa.grids.load_t_design(degree=2*N_sph)                
+                    tx = tgrid[:, 0]
+                    ty = tgrid[:, 1]
+                    tz = tgrid[:, 2]
+                    tazi, tcolat, _ = spa.utils.cart2sph(tx, ty, tz)
+
+                    R = spa.utils.rotation_euler(yaw, pitch, roll)
+                    tgrid_rot = (R @ tgrid.T).T             
+                    tx_rot = tgrid_rot[:, 0]
+                    ty_rot = tgrid_rot[:, 1]
+                    tz_rot = tgrid_rot[:, 2]
+                    tazi_rot, tcolat_rot, _ = spa.utils.cart2sph(
+                        tx_rot, ty_rot, tz_rot)
+                    
+                    shmat = spa.sph.sh_matrix(N_sph, tazi, tcolat, 'real')
+                    shmat_rotref = spa.sph.sh_matrix(tazi_rot, tcolat_rot)
+
+                    R = spa.sph.rotation_matrix(N_sph, yaw, pitch, roll, 'real',
+                                                False)
+
+                    shmat_rot = R @ shmat
+                    assert_allclose(shmat_rotref, shmat_rot)
+
+                    shmat_rot = spa.sph.rotate_sh(shmat)
+                    assert_allclose(shmat_rotref, shmat_rot)
+                    
